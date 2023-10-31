b5695e76539a09a0e6ba7af998e9ebeab811ca94
unknown <yrezehi@gmail.com>
2023-10-31 06:10:35 -0700

    clean: upp

---
+++ Business-State-Machine/Business-State-Machine/Controllers/StateMachineController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Business_State_Machine.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StateMachineController : ControllerBase
+    {
+       
+    }
+}
