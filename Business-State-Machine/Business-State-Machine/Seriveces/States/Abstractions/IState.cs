a9324fcf52d526bfa1a1d8a0067fac263077109a
unknown <yrezehi@gmail.com>
2023-10-31 18:04:26 -0700

    add: StateAction

---
+++ Business-State-Machine/Business-State-Machine/Seriveces/States/Abstractions/IState.cs
@@ -0,0 +1,8 @@
+﻿namespace Business_State_Machine.Seriveces.State.Abstractions
+{
+    public class IState
+    {
+        private int StateId { get; set }
+        private string StateName { get; set; }
+    }
+}
---
+++ Business-State-Machine/Business-State-Machine/Seriveces/States/StateAction.cs
@@ -0,0 +1,11 @@
+﻿namespace Business_State_Machine.Seriveces.State
+{
+    public enum StateAction
+    {
+        BACK = -2,
+        CANCEL = -1,
+        STALE = 0,
+        NEXT = 1,
+        COMPLETED = 2
+    }
+}
